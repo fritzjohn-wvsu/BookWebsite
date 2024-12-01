7d4f4b70853c3086c7155ad3db41fe5f089b5f71
fritzjohn-wvsu <fritzjohn.simpas@wvsu.edu.ph>
2024-12-01 18:14:52 +0800

    Updated and added new files

---
+++ lib/pages/bookDetails.dart
@@ -0,0 +1,262 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart'; // Import url_launcher
+
+class BookDetailPage extends StatefulWidget {
+  final String title;
+  final String? imageUrl;
+  final String description;
+  final String author;
+  final String publishedDate;
+  final String categories;
+  final String printType;
+  final String previewLink; // Added preview link field
+
+  const BookDetailPage({
+    Key? key,
+    required this.title,
+    this.imageUrl,
+    required this.description,
+    required this.author,
+    required this.publishedDate,
+    required this.categories,
+    required this.printType,
+    required this.previewLink, // Added preview link parameter
+  }) : super(key: key);
+
+  @override
+  _BookDetailPageState createState() => _BookDetailPageState();
+}
+
+class _BookDetailPageState extends State<BookDetailPage> {
+  bool _isExpanded = false;
+
+  // Function to launch the URL
+  Future<void> _launchURL() async {
+    final Uri url = Uri.parse(widget.previewLink);
+    if (await canLaunch(url.toString())) {
+      await launch(url.toString());
+    } else {
+      throw 'Could not launch ${widget.previewLink}';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 0, 15, 22),
+      appBar: AppBar(
+        title: const Text(
+          'Book Details',
+          style: TextStyle(color: Color(0xffe3eed4)),
+        ),
+        backgroundColor: const Color.fromARGB(255, 0, 15, 22),
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(100), // Adjusted padding to 50
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (widget.imageUrl != null)
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Container(
+                      color: Colors.white,
+                      child: Image.network(
+                        widget.imageUrl!,
+                        width: 200, // Adjusted width for better fitting
+                        height: 300, // Adjusted height for better fitting
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                const SizedBox(width: 25),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Text(
+                          widget.title,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ), // Adjusted spacing
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Description",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              widget.description,
+                              style: const TextStyle(
+                                color: Color(0xffe3eed4),
+                                fontSize: 16,
+                              ),
+                              textAlign: TextAlign.justify,
+                              maxLines: _isExpanded ? null : 3,
+                              overflow: _isExpanded
+                                  ? TextOverflow.visible
+                                  : TextOverflow.ellipsis,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Container(
+                                width: double.infinity,
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10.0),
+                                child: TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _isExpanded = !_isExpanded;
+                                    });
+                                  },
+                                  child: Text(
+                                    _isExpanded ? "Show Less" : "Show More",
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 15), // Adjusted spacing
+                      // Author and Published Date Row side by side
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Author: ',
+                              style: const TextStyle(
+                                color: Color(0xffe3eed4),
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                            Text(
+                              widget.author,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(width: 30),
+                            Text(
+                              'Published: ',
+                              style: const TextStyle(
+                                color: Color(0xffe3eed4),
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                            Text(
+                              widget.publishedDate,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      // Categories and Print Type Row side by side
+                      Row(
+                        children: [
+                          Text(
+                            'Categories: ',
+                            style: const TextStyle(
+                              color: Color(0xffe3eed4),
+                              fontSize: 16,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                          Text(
+                            widget.categories,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 30),
+                          Text(
+                            'Print Type: ',
+                            style: const TextStyle(
+                              color: Color(0xffe3eed4),
+                              fontSize: 16,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                          Text(
+                            widget.printType,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      // Display Preview Link
+                      SizedBox(height: 20), // Adjusted spacing
+                      const Text(
+                        'Preview Link: ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: _launchURL, // Make the link clickable
+                        child: Text(
+                          widget.previewLink,
+                          style: const TextStyle(
+                            color: Colors.blue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/booklist.dart
+++ lib/pages/booklist.dart
@@ -1,67 +1,228 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'navigation.dart'; // Import the navigation bar file
+import 'navigation.dart'; // Assuming this is a custom widget
+import 'bookDetails.dart'; // Assuming this is the BookDetailPage
 
-class BookList extends StatefulWidget {
+// Your Google Books API key
+const String googleBooksApiKey = 'AIzaSyBKsd3N8K0L4d6I-UZf5sOQE5LHWvdyPbk';
+
+class BookListWidget extends StatefulWidget {
   @override
-  _BookListPageState createState() => _BookListPageState();
+  _BookListWidgetState createState() => _BookListWidgetState();
 }
 
-class _BookListPageState extends State<BookList> {
-  List<dynamic> meals = [];
+class _BookListWidgetState extends State<BookListWidget> {
+  List<dynamic> allBooks = [];
+  List<dynamic> filteredBooks = [];
+  String selectedGenre = 'All'; // Genre filter
+  String selectedOrder = 'A-Z'; // Sorting order
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    fetchMeals(); // Fetch meals when the page is initialized
+    fetchBooks();
   }
 
-  // Fetch meal data from TheMealDB API
-  Future<void> fetchMeals() async {
-    final url =
-        //https://www.googleapis.com/books/v1/volumes?q=a&key=AIzaSyBKsd3N8K0L4d6I-UZf5sOQE5LHWvdyPbk
-        'https://www.themealdb.com/api/json/v1/1/search.php?f=b'; // API link for meals starting with 'A'
-    final response = await http.get(Uri.parse(url));
+  Future<void> fetchBooks() async {
+    final urls = [
+      'https://www.googleapis.com/books/v1/volumes?q=fiction&key=$googleBooksApiKey',
+      'https://www.googleapis.com/books/v1/volumes?q=non+fiction&key=$googleBooksApiKey',
+    ];
 
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.body);
+    try {
+      final responses = await Future.wait(
+        urls.map((url) async {
+          final response = await http.get(Uri.parse(url));
+          return response;
+        }),
+      );
+
+      // Check if the widget is still mounted before updating the state
+      if (!mounted) return;
+
       setState(() {
-        meals = data['meals'] ?? []; // Use the 'meals' data
+        // Combine the responses for both Fiction and Non-Fiction
+        allBooks = [];
+        for (var response in responses) {
+          if (response.statusCode == 200) {
+            final data = json.decode(response.body);
+            allBooks.addAll(data['items'] ?? []);
+          } else {
+            debugPrint('Error fetching books: ${response.statusCode}');
+          }
+        }
+        // Initially show all books
+        filteredBooks = List.from(allBooks);
+        isLoading = false;
       });
-    } else {
-      throw Exception('Failed to load meals');
+    } catch (e) {
+      if (!mounted) return;
+
+      setState(() {
+        isLoading = false;
+      });
+      debugPrint('Error fetching books: $e');
     }
+  }
+
+  // Function to filter books based on genre and order
+  void filterBooks() {
+    List<dynamic> filtered = allBooks;
+
+    // Filter by genre
+    if (selectedGenre != 'All') {
+      filtered = filtered.where((book) {
+        final categories = book['volumeInfo']['categories'] ?? [];
+        return categories.contains(selectedGenre);
+      }).toList();
+    }
+
+    // Sort the books based on the selected order
+    if (selectedOrder == 'A-Z') {
+      filtered.sort((a, b) =>
+          a['volumeInfo']['title'].compareTo(b['volumeInfo']['title']));
+    } else if (selectedOrder == 'Z-A') {
+      filtered.sort((a, b) =>
+          b['volumeInfo']['title'].compareTo(a['volumeInfo']['title']));
+    }
+
+    setState(() {
+      filteredBooks = filtered;
+    });
+  }
+
+  Widget _bookRectangle(
+      String bookName,
+      String? imageUrl,
+      String description,
+      String author,
+      String publishedDate,
+      String categories,
+      String printType,
+      String previewLink) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => BookDetailPage(
+              title: bookName,
+              imageUrl: imageUrl,
+              description: description,
+              author: author,
+              publishedDate: publishedDate,
+              categories: categories,
+              printType: printType,
+              previewLink: previewLink,
+            ),
+          ),
+        );
+      },
+      child: Column(
+        children: [
+          // Book Image
+          Container(
+            width: 200,
+            height: 300,
+            decoration: BoxDecoration(
+              color: const Color(0xffe3eed4),
+              borderRadius: BorderRadius.circular(10),
+              image: imageUrl != null
+                  ? DecorationImage(
+                      image: NetworkImage(imageUrl),
+                      fit: BoxFit.cover,
+                    )
+                  : null,
+            ),
+          ),
+          const SizedBox(height: 8),
+          // Book Title
+          SizedBox(
+            width: 200,
+            child: Text(
+              bookName,
+              style: const TextStyle(
+                color: Color(0xffe3eed4),
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.none,
+              ),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBookGrid(List<dynamic> bookList) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 5, // 5 items per row
+        crossAxisSpacing: 20.0, // Horizontal space
+        mainAxisSpacing: 10.0, // Vertical space
+        childAspectRatio: 200 / 300, // Width to height ratio
+      ),
+      itemCount: bookList.length,
+      itemBuilder: (context, index) {
+        final volumeInfo = bookList[index]['volumeInfo'];
+        final title = volumeInfo['title'] ?? 'Unknown Title';
+        final imageUrl = volumeInfo['imageLinks']?['thumbnail'];
+        final description =
+            volumeInfo['description'] ?? 'No description available';
+        final author =
+            volumeInfo['authors'] != null && volumeInfo['authors'].isNotEmpty
+                ? volumeInfo['authors'][0]
+                : 'Unknown Author';
+        final publishedDate = volumeInfo['publishedDate'] ?? 'Unknown Date';
+        final categories = volumeInfo['categories'] != null &&
+                volumeInfo['categories'].isNotEmpty
+            ? volumeInfo['categories'].join(', ')
+            : 'No categories available';
+        final printType = volumeInfo['printType'] ?? 'Unknown';
+        final previewLink = volumeInfo['previewLink'] ?? '';
+
+        return _bookRectangle(
+          title,
+          imageUrl,
+          description,
+          author,
+          publishedDate,
+          categories,
+          printType,
+          previewLink,
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 0, 15, 22),
       body: Column(
         children: [
-          navigationBar(context), // Call the navigationBar widget here
-          // Remove the SizedBox or Padding between navigation and BookList
+          navigationBar(context),
           Expanded(
-            child: meals.isEmpty
-                ? Center(
-                    child: CircularProgressIndicator(),
-                  ) // Show loading indicator
+            child: isLoading
+                ? const Center(child: CircularProgressIndicator())
                 : SingleChildScrollView(
-                    child: Container(
-                      padding: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 0, 15,
-                            22), // Set the container background color
-                      ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          // Title for the meals section
                           const Text(
                             "Book List",
                             style: TextStyle(
                               fontSize: 30,
-                              color: Color(0xffe3eed4), // Light text color
+                              color: Color(0xffe3eed4),
                               fontWeight: FontWeight.bold,
                             ),
                           ),
@@ -71,35 +232,46 @@ class _BookListPageState extends State<BookList> {
                             height: 20,
                           ),
                           const SizedBox(height: 25),
-                          // Row of small rectangles for meals (Meals 1 to 5)
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: meals
-                                .sublist(
-                                    0,
-                                    meals.length > 5
-                                        ? 5
-                                        : meals.length) // Safe sublist
-                                .map((meal) => _bookRectangle(
-                                    meal['strMeal'],
-                                    meal[
-                                        'strMealThumb'])) // Passing meal name and image URL
-                                .toList(),
+                            children: [
+                              // Genre Dropdown
+                              DropdownButton<String>(
+                                value: selectedGenre,
+                                onChanged: (newValue) {
+                                  setState(() {
+                                    selectedGenre = newValue!;
+                                  });
+                                  filterBooks();
+                                },
+                                items: ['All', 'Fiction', 'Non-Fiction']
+                                    .map((genre) {
+                                  return DropdownMenuItem<String>(
+                                    value: genre,
+                                    child: Text(genre),
+                                  );
+                                }).toList(),
+                              ),
+                              const SizedBox(width: 20),
+                              // Order Dropdown
+                              DropdownButton<String>(
+                                value: selectedOrder,
+                                onChanged: (newValue) {
+                                  setState(() {
+                                    selectedOrder = newValue!;
+                                  });
+                                  filterBooks();
+                                },
+                                items: ['A-Z', 'Z-A'].map((order) {
+                                  return DropdownMenuItem<String>(
+                                    value: order,
+                                    child: Text(order),
+                                  );
+                                }).toList(),
+                              ),
+                            ],
                           ),
                           const SizedBox(height: 25),
-                          // Row of small rectangles for meals (Meals 6 to 10)
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: meals.length > 5
-                                ? meals
-                                    .sublist(5,
-                                        meals.length > 10 ? 10 : meals.length)
-                                    .map((meal) => _bookRectangle(
-                                        meal['strMeal'], meal['strMealThumb']))
-                                    .toList()
-                                : [], // Empty row if less than 6 meals
-                          ),
-                          const SizedBox(height: 50),
+                          _buildBookGrid(filteredBooks),
                         ],
                       ),
                     ),
@@ -107,32 +279,6 @@ class _BookListPageState extends State<BookList> {
           ),
         ],
       ),
-    );
-  }
-
-  // Helper widget to create a rectangle for each meal inside a book-like style
-  Widget _bookRectangle(String mealName, String imageUrl) {
-    return Column(
-      children: [
-        Container(
-          width: 200,
-          height: 300,
-          color: const Color(0xffe3eed4), // Light color for the container
-          child: imageUrl != null && imageUrl.isNotEmpty
-              ? Image.network(imageUrl, fit: BoxFit.cover)
-              : const Icon(Icons.restaurant,
-                  size: 100, color: Colors.grey), // Default icon if no image
-        ),
-        const SizedBox(height: 10),
-        Text(
-          mealName,
-          style: const TextStyle(
-            color: Color(0xffe3eed4), // Text color
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ],
     );
   }
 }
